5428414687b77c92058548defd00a557fbd7d190
Eli112358 <eli112358.sarp@hotmail.com>
2014-10-18 14:35:56 -0400

    Added file gitAddFile.bat

---
+++ gitAddFile.bat
@@ -0,0 +1,17 @@
+﻿@echo off
+if "%~1"=="" goto help
+if "%~1"=="/?" goto help
+if not exist %1 goto fail
+echo Adding file...
+git add %1
+echo Committing...
+git commit -m "Added file %1"
+echo Publishing...
+git push
+echo Complete.
+exit /b
+:fail
+echo File not found.
+:help
+echo usage: %~dp0 path\to\file.ext
+exit /b
