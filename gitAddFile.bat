6dcafc3b52508a8c225a0506e298fda0c7054876
Eli112358 <eli112358.sarp@hotmail.com>
2014-10-18 14:43:08 -0400

    Fixed a few bugs and added a few features to gitAddFile.bat

--- gitAddFile.bat
+++ gitAddFile.bat
@@ -1,11 +1,17 @@
-﻿@echo off
+@echo off
+setlocal
 if "%~1"=="" goto help
 if "%~1"=="/?" goto help
 if not exist %1 goto fail
+set message="Added file %~1"
+if "%~2"=="" goto add
+if "%~3"=="" goto add
+if "%~2"=="-m" set message="%~3"
+:add
 echo Adding file...
 git add %1
 echo Committing...
-git commit -m "Added file %1"
+git commit -m %message%
 echo Publishing...
 git push
 echo Complete.
